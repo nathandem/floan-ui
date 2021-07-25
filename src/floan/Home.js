ffb4351ab9f51db72cfa47e0725e8a2e74f89806
nathandem <nathan.demaestri@gmail.com>
2021-07-25 15:19:45 +0200

    style: make Home looks better

--- src/floan/Home.js
+++ src/floan/Home.js
@@ -9,10 +9,12 @@ import './Home.css';
 export default function Home({ signedAddress, goToCreateLoanView, goToListLoansView }) {
     return (
         <div className="Home__container">
-            <h3>You are logged-in as {signedAddress}</h3>
+            <div>
+                <div><h3>You are logged-in as {signedAddress}</h3></div>
 
-            <Button variant="outlined" onClick={goToCreateLoanView}>Create loan request</Button>
-            <Button variant="outlined" onClick={goToListLoansView}>View open loans</Button>
+                <div className="Home__button"><Button variant="outlined" onClick={goToCreateLoanView}>Create loan request</Button></div>
+                <div className="Home__button"><Button variant="outlined" onClick={goToListLoansView}>View open loans</Button></div>
+            </div>
         </div>
     );
 }
